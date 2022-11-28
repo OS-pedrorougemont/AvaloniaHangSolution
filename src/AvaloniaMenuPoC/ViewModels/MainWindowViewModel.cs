ef6502350468cd14c5926b573f650eeddfbae67c
Pedro Rougemont <pedro.rougemont@outsystems.com>
2022-11-28 13:21:33 +0000

    MenuPoC with a disabled item prototype.

---
+++ src/AvaloniaMenuPoC/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Threading;
+using AvaloniaMenuPoC.Views;
+using ReactiveUI;
+
+namespace AvaloniaMenuPoC.ViewModels {
+    public class MainWindowViewModel : ReactiveObject {
+        private MainWindow _mainWindow;
+
+        public MainWindowViewModel(Window parent) {
+            OpenGuidedTourExampleCommand = ReactiveCommand.Create(
+                () => {
+                   _mainWindow = new MainWindow() { Background = Brushes.White };
+                    
+                    var innerContent = new Border() { BorderBrush = new SolidColorBrush(Colors.Red), BorderThickness = new Thickness(3)};
+                    
+                    _mainWindow.Content = innerContent;
+                }
+            );
+
+            ReactiveCommand.Create(
+                () => {
+                    _mainWindow.Close();
+                }
+            );
+        }
+
+        public MainWindowViewModel() {
+            
+        }
+
+        public ICommand OpenGuidedTourExampleCommand { get; }
+    }
+}
